0fd8a2abdc2926d3709da67a651919845b239d4e
Natsuki Ikeguchi <me@s6n.jp>
2024-02-11 14:26:13 +0900

    feat: Release 2.29.5374.100

--- Casks/mozc.rb
+++ Casks/mozc.rb
@@ -1,9 +1,9 @@
 cask "mozc" do
   arch arm: "arm64", intel: "intel64"
 
-  version "2.29.5268.102"
-  sha256 arm:   "fb6a0e2912840737ebd0e34ef5e733c5eb6d38c8a1f394784bcffbd87502af71",
-         intel: "4578d3eea085b19c38b4b69b3b70adabfe20515eb04f7262ff7ecce757872d0e"
+  version "2.29.5374.100"
+  sha256 arm:   "42572114861c50e59a137242d46a72f21ee61230324580da59c5bb836356305f",
+         intel: "80a28734df4eb94957d17bf238bea9efb340e38a4834421aa197904c4c450c89"
 
   url "https://github.com/siketyan/homebrew-mozc/releases/download/#{version}/Mozc_#{arch}.pkg.zip"
 
